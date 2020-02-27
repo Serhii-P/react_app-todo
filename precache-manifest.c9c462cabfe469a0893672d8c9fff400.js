4737a09e3892fde1e31a53d6855f98dfd83781cc
Serhii-P <serhii.pyskun@gmail.com>
2020-02-27 08:45:35 +0200

    Updates

---
+++ precache-manifest.c9c462cabfe469a0893672d8c9fff400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d8e4db66b584d4e005cc1e02b4b80a",
+    "url": "/react_app-todo/index.html"
+  },
+  {
+    "revision": "3fff968a86d4feb71850",
+    "url": "/react_app-todo/static/css/main.3581d148.chunk.css"
+  },
+  {
+    "revision": "0a0a77d5a05700cc2bff",
+    "url": "/react_app-todo/static/js/2.d85a316c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_app-todo/static/js/2.d85a316c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fff968a86d4feb71850",
+    "url": "/react_app-todo/static/js/main.a6512bd7.chunk.js"
+  },
+  {
+    "revision": "2afd7e2589d54cb71f13",
+    "url": "/react_app-todo/static/js/runtime-main.f82d4838.js"
+  }
+]);
